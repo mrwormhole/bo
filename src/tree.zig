7c32bcb1b98237bb90b196da4fb4d6b3f6f5b6f3
Talha Altinel <talhaaltinel@hotmail.com>
2026-05-03 19:50:46 +0100

    AI fix

--- src/tree.zig
+++ src/tree.zig
@@ -1059,7 +1059,7 @@ fn runWithArgv(gpa: std.mem.Allocator, argv_slice: [:null][*c]u8, io: std.Io, en
                                 return error.InvalidArgument;
                             }
                         }
-                        Level = std.fmt.parseInt(isize, std.mem.span(sLevel), 10) catch 0;
+                        Level = std.fmt.parseInt(isize, std.mem.span(sLevel), 0) catch 0;
                         Level -= 1;
                         if (Level < 0) {
                             _ = c.fprintf(c.Stderr(), "tree: Invalid level, must be greater than 0.\n");
